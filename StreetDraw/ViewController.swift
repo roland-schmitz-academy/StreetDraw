86e032bb8456161eb65c8c254a229f357e258066
Roland Schmitz <rola@2rs.de>
2019-11-17 17:43:27 +0100

    Initial Commit

---
+++ StreetDraw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreetDraw
+//
+//  Created by Roland Schmitz on 17.11.19.
+//  Copyright © 2019 Roland Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
