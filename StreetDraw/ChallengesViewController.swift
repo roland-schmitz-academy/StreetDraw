d0261b6cada9729e17f996f9c00fbf390c7cc882
pasqualemoscillo <pasqualemoscillo@users.noreply.github.com>
2019-11-19 10:52:25 +0100

    Walking man Icon fixed, challenges TableView with chapters from StreetDrawApplication

--- StreetDraw/ChallengesViewController.swift
+++ StreetDraw/ChallengesViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ChallengesViewController: UILoggingViewController {
-
+class ChallengesViewController: UILoggingViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var chaptersAndChallengesTableView: UITableView!
+    
     var streetDrawApp: StreetDrawApplication? {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.streetDrawApp
@@ -17,22 +20,43 @@ class ChallengesViewController: UILoggingViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        chaptersAndChallengesTableView.dataSource = self
         print("ChallengesViewController.viewDidLoad()")
-        print("streetDrawApp: \(streetDrawApp)")
+        print("streetDrawApp: \(String(describing: streetDrawApp?.chapters))")
         
         
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection \(section) ?")
+        return streetDrawApp?.chapters[section].challenges.count ?? 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt \(indexPath) ?")
+        return UITableViewCell()
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        print("numberOfSections() ?")
+        return streetDrawApp?.chapters.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        streetDrawApp?.chapters[section].name
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
