d207e5b1fb5b1e0e98f70e3c15c7618a02bbc618
Roland Schmitz <rola@2rs.de>
2019-11-20 08:54:35 +0100

    ShapeView added in Challenges Screen

---
+++ StreetDraw/ShapeView.swift
@@ -0,0 +1,26 @@
+//
+//  ShapeView.swift
+//  StreetDraw
+//
+//  Created by Roland Schmitz on 20.11.19.
+//  Copyright © 2019 Roland Schmitz. All rights reserved.
+//
+
+import UIKit
+
+class ShapeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func draw(_ rect: CGRect) {
+        print("draw")
+        
+    }
+
+}
