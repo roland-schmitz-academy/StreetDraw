f797dac03b5fd720c4ce2cbcf0978c621b58ccc8
pasqualemoscillo <pasqualemoscillo@users.noreply.github.com>
2019-11-21 12:39:33 +0100

    Add pentagon, hexagon, octagon, bottle and flute shapes

--- StreetDraw/StreetDrawApplicationFactory.swift
+++ StreetDraw/StreetDrawApplicationFactory.swift
@@ -44,16 +44,66 @@ func createStreetDrawApplication() -> StreetDrawApplication {
         CGPoint(x: 2,y: 1),
         CGPoint(x: 1.5,y: 0)
     ])
+    let pentagon = Shape(points: [
+        CGPoint(x: 1.5,y: 0),
+        CGPoint(x: 0,y: 1.25),
+        CGPoint(x: 0.65,y: 2.75),
+        CGPoint(x: 2.35,y: 2.75),
+        CGPoint(x: 3,y: 1.25),
+        CGPoint(x: 1.5,y: 0)
+    ])
+        let octagon = Shape(points: [
+        CGPoint(x: 0.75,y: 0),
+        CGPoint(x: 0,y: 0.75),
+        CGPoint(x: 0,y: 1.75),
+        CGPoint(x: 0.75,y: 2.5),
+        CGPoint(x: 1.75,y: 2.5),
+        CGPoint(x: 2.5,y: 1.75),
+        CGPoint(x: 2.5, y: 0.75),
+        CGPoint(x: 1.75,y: 0),
+        CGPoint(x: 0.75,y: 0)
+    ])
+        let hexagon = Shape(points: [
+        CGPoint(x: 0.75,y: 0),
+        CGPoint(x: 0,y: 0.5),
+        CGPoint(x: 0,y: 1.25),
+        CGPoint(x: 0.75,y: 1.75),
+        CGPoint(x: 1.5,y: 1.25),
+        CGPoint(x: 1.5,y: 0.5),
+        CGPoint(x: 0.75,y: 0)
+    ])
+    let bottle = Shape(points: [
+        CGPoint(x: 0.25,y: 0),
+        CGPoint(x: 0.25,y: 0.25),
+        CGPoint(x: 0,y: 1.25),
+        CGPoint(x: 0,y: 3),
+        CGPoint(x: 0.75,y: 3),
+        CGPoint(x: 0.75,y: 1.25),
+        CGPoint(x: 0.5, y: 0.25),
+        CGPoint(x: 0.5,y: 0),
+        CGPoint(x: 0.25,y: 0)
+    ])
+    let flute = Shape(points: [
+        CGPoint(x: 0,y: 0),
+        CGPoint(x: 1,y: 1.5),
+        CGPoint(x: 1,y: 2.75),
+        CGPoint(x: 0.5,y: 3),
+        CGPoint(x: 1.75,y: 3),
+        CGPoint(x: 1.25,y: 2.75),
+        CGPoint(x: 1.25, y: 1.5),
+        CGPoint(x: 2.25,y: 0),
+        CGPoint(x: 0,y: 0)
+    ])
     
     // todo: define more shapes
     
     return StreetDrawApplication(chapters: [
         Chapter(name: "Test", challenges: [
-            Challenge(name: "Small Square", shape: square, minDistance: 100.0, maxDuration: 20.5, standardScore: 100, penaltyScore: nil, difficulty: .easy ),
-            Challenge(name: "Small Rectangle", shape: rectangle, minDistance: 100.0, maxDuration: nil, standardScore: 100, penaltyScore: nil, difficulty: .easy ),
+            Challenge(name: "Small Square", shape: bottle, minDistance: 100.0, maxDuration: 20.5, standardScore: 100, penaltyScore: nil, difficulty: .easy ),
+            Challenge(name: "Small Rectangle", shape: flute, minDistance: 100.0, maxDuration: nil, standardScore: 100, penaltyScore: nil, difficulty: .easy ),
             Challenge(name: "Small Rectangle", shape: rhombus, minDistance: 100.0, maxDuration: nil, standardScore: 100, penaltyScore: nil, difficulty: .easy ),
             Challenge(name: "Big Square 2", shape: star, minDistance: 100.0, maxDuration: 20.5, standardScore: 100, penaltyScore: nil, difficulty: .normal ),
-            Challenge(name: "Big Rectangle 2", shape: rectangle, minDistance: 100.0, maxDuration: nil, standardScore: 100, penaltyScore: nil, difficulty: .hard),
+            Challenge(name: "Big Rectangle 2", shape: hexagon, minDistance: 100.0, maxDuration: nil, standardScore: 100, penaltyScore: nil, difficulty: .hard),
         ]),
         
         
@@ -63,7 +113,7 @@ func createStreetDrawApplication() -> StreetDrawApplication {
         ]),
         Chapter(name: "Warmup", challenges: [
             Challenge(name: "Square", shape: square, minDistance: 0.5, maxDuration: nil, standardScore: 1000, penaltyScore: nil, difficulty: .easy ),
-            Challenge(name: "Rectangle", shape: rectangle, minDistance: 0.5, maxDuration: nil, standardScore: 1000, penaltyScore: nil, difficulty: .easy )
+            Challenge(name: "Rhombus", shape: rhombus, minDistance: 0.5, maxDuration: nil, standardScore: 1000, penaltyScore: nil, difficulty: .easy )
         ]),
         
         Chapter(name: "Get ready", challenges: [
