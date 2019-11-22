37b4a4cae4adb5c53160eaccf62286afba29f1c9
Tareq Saifullah <tareqsaifu@gmail.com>
2019-11-22 14:13:32 +0100

    deleted interactionwithplist and named it persistData

---
+++ persistData.swift
@@ -0,0 +1,13 @@
+//
+//  persistData.swift
+//  StreetDraw
+//
+//  Created by Tareq Saifullah on 22/11/2019.
+//  Copyright © 2019 Roland Schmitz. All rights reserved.
+//
+
+import Foundation
+
+func createAchievement() {
+//    do something
+}
