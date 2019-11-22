b8cdb64c4b864784c2ada31d04527956b8dca52a
Tareq Saifullah <tareqsaifu@gmail.com>
2019-11-22 13:54:40 +0100

    Created a file to interact with the plist
    Created a file to interact with the plist

---
+++ StreetDraw/interactionWithPlist.swift
@@ -0,0 +1,21 @@
+//
+//  interactionWithPlist.swift
+//  StreetDraw
+//
+//  Created by Tareq Saifullah on 22/11/2019.
+//  Copyright © 2019 Roland Schmitz. All rights reserved.
+//
+
+import Foundation
+
+func saveAchivements(){
+    print("hello")
+    //takes the value of achivement
+    
+    
+    // saves the value in plist
+    
+    
+    // Sorts the value
+    
+}
