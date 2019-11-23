7c1235078631f4e332a2a86a63f430dd6da53ac8
Tareq Saifullah <tareqsaifu@gmail.com>
2019-11-23 20:38:48 +0100

    modified version of stop watch

--- StreetDraw/Stopwatch.swift
+++ StreetDraw/Stopwatch.swift
@@ -11,43 +11,57 @@ var startingTime:TimeInterval = 0.0
 var stopingTime : TimeInterval = 0.0
 var lastPauseTime: TimeInterval = 0.0
 var totalPauseTime : TimeInterval = 0.0
+var activeDur: TimeInterval = 0.0
+var isCont: Int = 0
 
 
 class Stopwatch {
     func start() {
-        // todo
         startingTime = Date().timeIntervalSince1970
-        
     }
     
     func stop() {
         stopingTime = Date().timeIntervalSince1970
+        isCont = 0
+        activeDur = stopingTime-startingTime-totalPauseTime
+        
     }
     
     func pause() {
         // todo
         lastPauseTime = Date().timeIntervalSince1970
-        
+        isCont = 0
+        activeDur = lastPauseTime - startingTime - totalPauseTime
         
         
         
     }
     
     func `continue`() {
-        totalPauseTime += Date().timeIntervalSince1970 - lastPauseTime
+        let cont = Date().timeIntervalSince1970
+        totalPauseTime += cont - lastPauseTime
         lastPauseTime = 0.0
+        isCont = 1
     }
     
     var overallDuration: TimeInterval {
-        // todo
-         return stopingTime - startingTime
+        return stopingTime - startingTime
         
     }
 
     var activeDuration: TimeInterval {
-        var now = Date().timeIntervalSince1970
-        return now - startingTime-totalPauseTime
-
+     if (activeDur == 0.0) {
+                activeDur = Date().timeIntervalSince1970 - startingTime
+                return activeDur
+            
+            }else if(isCont == 1 ){
+                
+                activeDur = Date().timeIntervalSince1970 - startingTime - totalPauseTime
+                return activeDur
+            }else{
+                return activeDur
+            }
     }
+    
 
 }
